be5213e271b79d6e2ca746b58a972103e305dafa
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-09-04 21:32:49 +0000

    git push

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,7 +1,169 @@
-
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiJavascript } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { Link } from "react-router-dom";
+import { GoArrowUpRight } from "react-icons/go";
 const Project = () => {
   return (
-    <div>Project</div>
+    <div className="h-[100vh] px-4 md:px-40 py-5 text-white">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 items-center justify-items-center">
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+        <div className="bg-white text-black p-6 rounded-2xl h-fit w-fit flex gap-4 flex-col shadow hover:shadow-xl">
+          <div className="rounded-xl bg-white p-2 h-[250px] w-[250px] md:w-[450px]" style={{
+            backgroundImage: `url('https://devakshay.vercel.app/assets/img/swiftrevel.webp')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+          }}></div>
+          <h2 className="font-poppins px-2 text-2xl font-medium">Project Name</h2>
+          <div className="flex gap-4 px-2">
+            <FaReact size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiJavascript size={24} className="text-sky-500 hover:scale-110 duration-300" />
+            <RiTailwindCssFill size={28} className="text-sky-500 hover:scale-110 duration-300" />
+            <SiMongodb size={28} className="text-sky-500 hover:scale-110 duration-300" />
+          </div>
+          <div className="flex relative gap-4 items-center">
+            <Link className="bg-gray-100 p-4 rounded-full border-2 border-gray-300 hover:border-4"><GoArrowUpRight size={20}/></Link>
+            <Link className="rounded-full p-4 w-12 h-12" style={{
+            backgroundImage: `url('https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg')`,
+            backgroundPosition: 'center',
+            backgroundSize:'cover',
+          }}></Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
